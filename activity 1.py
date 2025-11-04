f34d2eac8dae783b899eb786e901fd26d88469d9
harinivassahana-max <harinivassahana@gmail.com>
2025-11-04 16:20:13 +1100

    activity 1

---
+++ activity 1.py
@@ -0,0 +1,15 @@
+#take input for the student that he cab attend the exam or not 
+medical_cause=input("did you have a medical cause Y or N:")
+#take input of the attendance
+atten = int(input("enter the attendance of the stdent:"))
+
+#checking the user input predicting output accordingly
+
+if medical_cause == 'Y': #checking the condition 1
+    print("you are allowed")
+else:
+    if atten>75: #checking the condition 1
+      print("allowed")
+    else:
+      print("not allowed")
+
