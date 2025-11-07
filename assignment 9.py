a5003dc48e1d9dce1f4b1ec91d5c7892bea1a761
harinivassahana-max <harinivassahana@gmail.com>
2025-11-07 16:35:33 +1100

    assignment 9

---
+++ assignment 9.py
@@ -0,0 +1,8 @@
+i = 13
+if (i==13):
+    if(i<15):
+        print("i is smaller than 15")
+    if(i<13):
+        print("i is smaller than 13 too")
+    else:
+        print("i is greater than 12 and smaller than 15")
